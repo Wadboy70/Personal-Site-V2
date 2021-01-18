320fb92e9ee4eb7985c151dbb08160c273e35366
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2021-01-18 15:55:46 -0500

    email  bar done

--- src/components/Sidebar/EmailSidebar.tsx
+++ src/components/Sidebar/EmailSidebar.tsx
@@ -6,12 +6,14 @@ import Sidebar from './Sidebar';
 const EmailBox = styled.a`
     display: flex;
     font-size: 3.5vh;
+    font-family: 'Roboto Mono', serif;
     writing-mode: vertical-rl;
+    text-decoration: none;
 `;
 const EmailSidebar = () =>{
 	return(
         <Sidebar left = { false }>
-            <EmailBox className = 'growOnHover'>
+            <EmailBox className = 'growOnHover' href = 'mailto:s.oluwaleimu@gmail.com'>
                 s.oluwaleimu@gmail.com
             </EmailBox>
         </Sidebar>
